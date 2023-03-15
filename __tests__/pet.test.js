a020e679f2ae0e4e8626d69506f16a07aaf7be55
Max Tauroza <maxtauroza@gmail.com>
2023-03-15 12:38:28 +0000

    added hunger and fitness proprties which are also part of growUp prototype, wrote tests and passed

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,4 @@
-const { describe } = require('node:test');
-const {
-    Pet
- } = require('../src/pet');
-
+const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
@@ -12,18 +8,52 @@ describe('constructor', () => {
 
 describe('Pet has a name property', () => {
     it('returns the name of the specified pet', () => {
-        const pet = new Pet ('Fido');
+        const pet = new Pet('Fido');
 
         expect(pet.name).toEqual('Fido');
     });
 });
 
-describe('prototype', () => {
+describe('aging', () => {
+    it('Has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.age).toBe(0);
+    })
+
     it('causes the pet to age by 1', () => {
-        const pet = new Pet ('Fido');
+        const pet = new Pet('Fido');
         pet.growUp();
         pet.growUp();
 
         expect(pet.age).toEqual(2);
+    });
+});
+
+describe('hunger', () => {
+    it('Has an initial hunger value of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+
+    it('growUp increases hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+});
+
+describe('fitness', () => {
+    it('Has an initial fitness value of 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    });
+
+    it('growUp decreases fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
     });
 });
